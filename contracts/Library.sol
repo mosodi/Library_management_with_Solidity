23bdaf4fea94f00b8688a8477c07a0910aea486d
mosodi <itsmartinsevans@gmail.com>
2022-08-13 19:22:26 +0100

    Set finished

--- contracts/Library.sol
+++ contracts/Library.sol
@@ -2,13 +2,14 @@ pragma solidity ^0.8.0;
 
 contract Library {
 
-  event AddBook(address recipient, unit bookId)
+  event AddBook(address recipient, unit bookId);
+  event setFinished(uint bookId, bool finished);
 
   struct Book {
 
-    unint id;
+    uint id;
     string name;
-    unint year;
+    uint year;
     string author;
     bool finished;
 
@@ -18,19 +19,19 @@ book[] private booklist;
 
 //Mapping of Book id to the wallet address of the user adding the new book under their name
 
-  mapping(unint256 => address) bookToOwner;
+  mapping(uint256 => address) bookToOwner;
 
-  function addBook(string name, unint16 year, string memory author, bool finished) external {
-    unint bookId = bookList.length;
+  function addBook(string name, uint16 year, string memory author, bool finished) external {
+    uint bookId = bookList.length;
     bookList.push(Book(bookId, name, year, author, finished));
     bookToOwner[bookId] = msg.sender;
     emit AddBook(msg.sender, bookId)
   }
   function _getBookList(bool finished) private view returns (Book[] memory){
     Book[] memory temporary = new Book[] (bookList.length);
-    unint counter = 0;
+    uint counter = 0;
     for (unit i=0; i<bookList.lenght; i++){
-      if (bookToOwner[i] == msg.sender) {
+      if (bookToOwner[i] == msg.sender && bookList[i].finish == finished) {
         temporary[counter] = bookList[i];
         counter++;
       }
@@ -46,10 +47,17 @@ book[] private booklist;
 
   function getFinishedBook() external view returns (Book[] memory) {
     return _getBookList(true);
-    
+
   }
     function getUnifinishedBook() external view returns (Book[] memory) {
       return _getBookList(false)
 
+    }
+
+    function setFinished(uinit bookId, bool finished) external {
+      if(bookToOwner[bookID] == msg.sender) {
+        bookList[bookId].finished = finished;
+        emit SetFinished(bookId, finished);
+      }
     }
 }
